00e18dee28a22bf0dc4998dafa34c6775ca452f9
Avijot-singh <singhavijot17@gmail.com>
2023-12-08 14:28:47 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.CompilerServices;
+bool isSuccessful = true;
+while (isSuccessful)
+{
+    
+    
+    static void Taxi()
+    {
+        Console.WriteLine("Please enter your Taxi pickup spot:");
+        string TaxiP = Console.ReadLine();
+        Console.WriteLine("Please enter your uber destination:");
+        string TaxiI = Console.ReadLine();
+        Console.WriteLine($"Your Taxi from {TaxiP} to {TaxiI} has been confirmed");
+        Console.WriteLine("Total being $200");
+    }
+    static void Train()
+    {
+
+        Console.WriteLine("Please enter your Train pickup spot:");
+        string TrainP = Console.ReadLine();
+        Console.WriteLine("Please enter your Train destination:");
+        string TrainI = Console.ReadLine();
+        Console.WriteLine($"Your Taxi from {TrainP} to {TrainI} has been confirmed");
+        Console.WriteLine("Total being $15");
+    }
+    static void Bus()
+    {
+        Console.WriteLine("Please enter your Bus pickup spot:");
+        string BusP = Console.ReadLine();
+        Console.WriteLine("Please enter your Bus destination:");
+        string BusI = Console.ReadLine();
+        Console.WriteLine($"Your Taxi from {BusP} to {BusI} has been confirmed");
+        Console.WriteLine("Total being $200");
+    }
+    
+    
+    Console.WriteLine("Do you confirm ?");
+    string response = Console.ReadLine();
+    if (response == "yes" || response == "y")
+    {
+        Console.WriteLine("--------CONFIRMED--------");
+        isSuccessful = false;
+    }
+    else
+    {
+        Console.WriteLine("Confirmed Unsecuessfull");
+    }
+}
---
+++ TransportService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net
+{
+
+
+
+    public class TransportService
+    {
+        // Properties
+        public string PickupSpot { get; set; }
+        public string Destination { get; set; }
+        public string TotalCost { get; private set; }
+
+
+        private string userInput;
+
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine("Menu Display");
+            Console.WriteLine("1.   Uber");
+            Console.WriteLine("2.   Taxi");
+            Console.WriteLine("3.   Train");
+            Console.WriteLine("4.   Bus");
+            Console.WriteLine("---------------------------------------------");
+
+             userInput = Console.ReadLine();
+
+            if(int.TryParse(userInput, out int user))
+            {
+                switch(user)
+                {
+                    case 1:
+                        BookService();
+                        break;
+                }
+            }
+           
+
+        }
+
+        public virtual void BookService() // You create this method with virtual keyword so you can use it for ovveride and leave the body empty
+        {
+            Console.WriteLine("This is the base implementation for booking service");
+            
+        }
+        
+
+        public void ProcessServiceSelection()
+        {
+            
+        }
+
+        
+     
+
+
+
+    }
+    public class Uber : TransportService
+    {
+        public override void BookService()
+        {
+            Console.WriteLine("Please enter your uber pickup spot:");
+             PickupSpot = Console.ReadLine();
+            Console.WriteLine("Please enter your uber destination:");
+             Destination = Console.ReadLine();
+            Console.WriteLine($"Your uber from {PickupSpot}to {Destination} has been confirmed");
+            Console.WriteLine("Total being $200");
+
+            
+
+        }
+
+
+    }
+}
