37ffa80df67994e7188f0da73bd188af12a569b7
Avijot-singh <singhavijot17@gmail.com>
2023-12-09 18:11:38 +1100

    Added complete functionality of Uber service

--- TransportService.cs
+++ TransportService.cs
@@ -1,83 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace net
 {
-
-
-
     public class TransportService
     {
+
+        protected bool keepPlaying = true;
+
+
+
+
+
+
         // Properties
         public string PickupSpot { get; set; }
         public string Destination { get; set; }
-        public string TotalCost { get; private set; }
+        public string TotalCost { get; protected set; }
 
 
         private string userInput;
 
-
+      
         public void DisplayMenu()
         {
-            Console.WriteLine("Menu Display");
-            Console.WriteLine("1.   Uber");
-            Console.WriteLine("2.   Taxi");
-            Console.WriteLine("3.   Train");
-            Console.WriteLine("4.   Bus");
-            Console.WriteLine("---------------------------------------------");
-
-             userInput = Console.ReadLine();
-
-            if(int.TryParse(userInput, out int user))
-            {
-                switch(user)
-                {
-                    case 1:
-                        BookService();
-                        break;
-                }
-            }
            
+            
 
+                Console.WriteLine("Menu Display");
+                Console.WriteLine("1.   Uber");
+                Console.WriteLine("2.   Taxi");
+                Console.WriteLine("3.   Train");
+                Console.WriteLine("4.   Bus");
+                Console.WriteLine("---------------------------------------------");
+
+
+                Console.Write("Please enter your input:");
+                userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int user))
+                {
+                    switch (user)
+                    {
+                        case 1:
+                            BookService();
+                            break;
+                    }
+
+                }
+
+
+            
         }
+
 
         public virtual void BookService() // You create this method with virtual keyword so you can use it for ovveride and leave the body empty
         {
-            Console.WriteLine("This is the base implementation for booking service");
-            
+           
+
+
         }
-        
-
-        public void ProcessServiceSelection()
-        {
-            
-        }
-
-        
-     
-
 
 
     }
+
     public class Uber : TransportService
     {
         public override void BookService()
         {
             Console.WriteLine("Please enter your uber pickup spot:");
-             PickupSpot = Console.ReadLine();
+            PickupSpot = Console.ReadLine();
             Console.WriteLine("Please enter your uber destination:");
-             Destination = Console.ReadLine();
-            Console.WriteLine($"Your uber from {PickupSpot}to {Destination} has been confirmed");
-            Console.WriteLine("Total being $200");
+            Destination = Console.ReadLine();
+            TotalCost = "$40";
 
-            
+            Console.WriteLine($"Your uber from {PickupSpot} to {Destination} has been qued");
+            Console.WriteLine($"Total cost being {TotalCost}");
+
+            Console.WriteLine("Do you confirm the order");
+            string Confirm = Console.ReadLine().ToLower();
+            if (Confirm == "yes" || Confirm == "y") 
+            {
+                Console.WriteLine("---------CONFIRMED----------");
+            }
+            else
+            {
+                Console.WriteLine("-------ORDER NOT CONFIRMED---------");
+            }
+
+
+
+
 
         }
 
 
     }
 }
+
+
